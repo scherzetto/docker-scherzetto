b9f5f65332e74e0c3c716bd383d708f969dc1829
Benjamin Zaslavsky <benjamin.zaslavsky@manomano-externe.com>
2018-05-15 18:01:32 +0200

    MessageTrait

--- src/Lib/Http/Message/MessageTrait.php
+++ src/Lib/Http/Message/MessageTrait.php
@@ -78,4 +78,135 @@ trait MessageTrait
     {
         return implode(', ', $this->getHeader($header));
     }
+
+    /**
+     * @param string $header
+     * @param mixed $value
+     * @return $this
+     */
+    public function withHeader($header, $value)
+    {
+        if (!is_array($value)) {
+            $value = [$value];
+        }
+
+        $value = $this->trimHeaderValues($value);
+        $lowered = strToLower($header);
+
+        $clone = clone $this;
+        if (isset($clone->headerNames[$lowered])) {
+            unset i$clone->headers[$clone->headerNames[$lowered]];
+        }
+        $clone->headerNames[$lowered] = $header;
+        $clone->headers[$header] = $value;
+
+        return $clone;
+    }
+
+    /**
+     * @param string $header
+     * @param mixed $value
+     * @return $this
+     */
+    public function withAddedHeader($header, $value)
+    {
+        if (!is_array($value)) {
+            $value = [$value];
+        }
+
+        $value = $this->trimHeaderValues($value);
+        $lowered = strToLower($header);
+
+        $clone = clone $this;
+        if (isset($clone->headerNames[$lowered])) {
+            $header = $this->headerNames[$lowered];
+            $clone->headers$header] = array_merge($this->headers[$header], $value);
+        } else {
+            $clone->headerNames[$lowered] = $header;
+            $clone->headers[$header] = $value;
+        }
+
+        return $clone;
+    }
+
+    /**
+     * @param string $header
+     * @return $this
+     */
+    public function withouthHeader($header)
+    {
+        $lowered = strtolower($header);
+
+        if (!isset($this->headerNames($lowered))) {
+            return $this;
+        }
+
+        $header = $this->headerNames[$lowered];
+
+        $clone = clone $this;
+        unser($clone->headers[$header], $clone->headerNames[$lowered]);
+        return null;
+    }
+
+    /**
+     * @return StreamInterface
+     */
+    public function getBody()
+    {
+        if (!$this->stream) {
+            $this->stream = stream_for('');
+        }
+        return $this->stream;
+    }
+
+    /**
+     * @param StreamInterface $body
+     * @return $this
+     */
+    public function withBody(StreamInterface $body)
+    {
+        if ($body === $this->body) {
+            return $this;
+        }
+
+        $clone = clone $this;
+        $clone->stream = $body;
+        return $clone;
+    }
+
+    /**
+     * @param array $headers
+     * @return void
+     */
+    public function setHeaders(array $headers)
+    {
+        $this->headerNames = $this->headers = [];
+        foreach ($headers as $header => $value) {
+            if (!is_array($value)) {
+                $value = [$value];
+            }
+
+            $value = $this->trimHeaderValues($value);
+            $lowered = strtolower($header);
+
+            if (isset($clone->headerNames[$lowered])) {
+                $header = $this->headerNames[$lowered];
+                $clone->headers$header] = array_merge($this->headers[$header], $value);
+            } else {
+                $clone->headerNames[$lowered] = $header;
+                $clone->headers[$header] = $value;
+            }
+        }
+    }
+
+    /**
+     * @param string[] $values
+     * @return string[]
+     */
+    public function trimHeaderValues(array $values)
+    {
+        return array_map(function ($value) {
+            return trim($value, " \t");
+        }, $values);
+    }
 }
