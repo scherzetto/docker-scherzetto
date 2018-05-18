b2f25defd23ddc687171a21daa83be7861b3e680
Benjamin Zaslavsky <benjamin.zaslavsky@manomano-externe.com>
2018-05-18 16:43:20 +0200

    Skeleton for test

---
+++ src/MyBlog/App/Router.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MyBlog\App;
+
+use GuzzleHttp\Psr7\Request;
+
+class Router
+{
+    /**
+     * undocumented function
+     *
+     * @return void
+     */
+    public function route(Request $request)
+    {
+        return null;
+    }
+}
