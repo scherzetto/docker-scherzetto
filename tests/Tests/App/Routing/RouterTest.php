f95595c315d77811cb9a9a04df082257110838c6
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2018-05-21 15:55:22 +0200

    Add Phpunit tests and travis file

---
+++ tests/Tests/App/Routing/RouterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\App\Routing;
+
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase
+{
+    public function testContinuousIntegration()
+    {
+        $this->assertTrue(true);
+    }
+}
