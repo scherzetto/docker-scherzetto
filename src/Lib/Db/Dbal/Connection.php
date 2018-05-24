17f11594203248304f6453b35413e3b640033820
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2018-05-24 20:14:04 +0200

    Fix useless array_walk

--- src/Lib/Db/Dbal/Connection.php
+++ src/Lib/Db/Dbal/Connection.php
@@ -15,17 +15,14 @@ class Connection extends PDO
         $dsn      = $config['type'].';';
         $userpass = [];
 
-        $config = array_walk(
-            $config,
-            function ($val, $key) use ($keys, $userpass) {
-                if (!\in_array($key, $keys)) {
-                    if (\in_array($key, ['username', 'password'])) {
-                        $userpass[$key] = $val;
-                    }
-                    unset($config[$key]);
+        foreach ($config as $key => val) {
+            if (!\in_array($key, $keys)) {
+                if (\in_array($key, ['username', 'password'])) {
+                    $userpass[$key] = $val;
                 }
+                unset($config[$key]);
             }
-        );
+        }
         foreach ($keys as $key) {
             if (isset($config[$key])) {
                 $dsn .= "{$key}={$config[$key]};";
