8db0fda21bbc160f0976bdb8d7001cfa50b4c7b0
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2018-05-24 22:13:51 +0200

    Code smells fix attempt

--- src/Lib/Access/Accessor.php
+++ src/Lib/Access/Accessor.php
@@ -39,23 +39,19 @@ class Accessor
         $refObject = new \ReflectionClass(get_class($target));
         $camelized = $this->camelize($property);
 
-        $magicGet  = '__'.self::PREF_GET;
-        $magicSet  = '__'.self::PREF_SET;
-        $magicCall = '__call';
-
         $methods = [
             self::MODE_READ  => [
                 self::PREF_GET.$camelized => self::PREF_GET.$camelized,
                 self::PREF_HAS.$camelized => self::PREF_HAS.$camelized,
                 self::PREF_IS.$camelized  => self::PREF_IS.$camelized,
                 lcfirst($camelized)       => lcfirst($camelized),
-                $magicGet                 => self::PREF_GET.$camelized,
-                $magicCall                => self::PREF_GET.$camelized
+                '__'.self::PREF_GET       => self::PREF_GET.$camelized,
+                '__call'                  => self::PREF_GET.$camelized
             ],
             self::MODE_WRITE => [
                 self::PREF_SET.$camelized => self::PREF_SET.$camelized,
-                $magicSet                 => self::PREF_SET.$camelized,
-                $magicCall                => self::PREF_SET.$camelized
+                '__'.self::PREF_SET       => self::PREF_SET.$camelized,
+                '__call'                  => self::PREF_SET.$camelized
             ]
         ];
 
