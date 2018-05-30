510e9886341682bbe35707c2c56b1945cee6914a
Benjamin Zaslavsky <benjamin.zaslavsky@manomano-externe.com>
2018-05-30 16:56:08 +0200

    First draft at Dbal wrapper, tests coming

---
+++ src/Lib/Db/Dbal/Dbal.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Lib\Db\Dbal;
+
+class Dbal
+{
+    private $conn;
+
+    public function __construct(?Connection $conn)
+    {
+        $this->conn = $conn ?? new Connection();
+    }
+
+    public function beginTransaction()
+    {
+        return $this->conn->beginTransaction();
+    }
+
+    public function commit()
+    {
+        return $this->conn->commit();
+    }
+
+    public function exec(string $statement)
+    {
+        return $this->conn->exec($statement);
+    }
+
+    public function lastInsertId(string $name = null)
+    {
+        return $this->conn->lastInsertId($name);
+    }
+
+    public function prepare(string $statement, array $driverOptions = [])
+    {
+        return $this->conn->prepare($statement, $driverOptions);
+    }
+
+    public function query(string $statement, int $fetch = null, $arg3 = null, $arg4 = null)
+    {
+        if ($fetch !== null && $arg3 && $arg4) {
+            return $this->conn->query($statement, $fetch, $arg3, $arg4);
+        } elseif ($fetch !== null && $arg3) {
+            return $this->conn->query($statement, $fetch, $arg3);
+        }
+        return $this->conn->query($statement);
+    }
+
+    public function rollback()
+    {
+        return $this->conn->rollback();
+    }
+}
